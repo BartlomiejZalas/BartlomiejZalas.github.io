4f678b225204fd50b4cde85ea51e8a705824b742
Bartlomiej Zalas <bartekzalas@gmail.com>
2018-10-21 00:03:20 +0300

    init

---
+++ precache-manifest.1f385dd638026c55dd8d600923aeea3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "765e3d704079901c8c205c1e8c6f7320",
+    "url": "/static/media/home.765e3d70.jpg"
+  },
+  {
+    "revision": "84d56f2792d7edb96ed40a6f83354452",
+    "url": "/static/media/boats.84d56f27.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c022a4d6f9e3428c7cda",
+    "url": "/static/js/main.c022a4d6.chunk.js"
+  },
+  {
+    "revision": "9d4bc979319e91fbfb43",
+    "url": "/static/js/1.9d4bc979.chunk.js"
+  },
+  {
+    "revision": "c022a4d6f9e3428c7cda",
+    "url": "/static/css/main.9372dc5a.chunk.css"
+  },
+  {
+    "revision": "9d4bc979319e91fbfb43",
+    "url": "/static/css/1.da5dd65b.chunk.css"
+  },
+  {
+    "revision": "5ca972a38110f52f07e4688a9e09c5ac",
+    "url": "/index.html"
+  }
+];
